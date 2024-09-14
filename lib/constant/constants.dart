1ca6f424f7128bd405d23fc22e288875597a7920
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-14 14:26:15 +0300

    Added validation to values ​​in the form and fixed screen width

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const seedColor = Colors.green;
 const darkThemeTextColor = Colors.white;
+const double maxWidth = 420.0;
--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constant/constants.dart';
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
@@ -8,99 +10,148 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
   var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassqord = '';
+
+  void _submit() {
+    final formState = _formKey.currentState;
+
+    if (formState == null) {
+      return;
+    }
+
+    final isValid = formState.validate();
+    if (!isValid) {
+      return;
+    }
+
+    formState.save();
+
+    if (_isLogin) {
+    } else {}
+  }
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.sizeOf(context);
+    final double width = size.width;
+
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(20),
-                width: 200,
-                child: Image.asset('assets/images/chat.png'),
-              ),
-              Card(
-                margin: const EdgeInsets.all(20),
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Form(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: 'E-Mail',
+          child: SizedBox(
+            width: (width < maxWidth) ? double.infinity : maxWidth,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  width: 200,
+                  child: Image.asset('assets/images/chat.png'),
+                ),
+                Card(
+                  margin: const EdgeInsets.all(20),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'E-Mail',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter a valid emmail address';
+                                }
+                                return null;
+                              },
+                              onSaved: (newValue) {
+                                _enteredEmail = newValue!;
+                              },
                             ),
-                            keyboardType: TextInputType.emailAddress,
-                            autocorrect: false,
-                            textCapitalization: TextCapitalization.none,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: 'Password',
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                              ),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value == null || value.trim().length < 6) {
+                                  return 'Password must be at least 6 characters long';
+                                }
+
+                                return null;
+                              },
+                              onSaved: (newValue) {
+                                _enteredPassqord = newValue!;
+                              },
                             ),
-                            obscureText: true,
-                          ),
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context)
-                                  .colorScheme
-                                  .onPrimaryContainer,
+                            const SizedBox(height: 20),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .onPrimaryContainer,
+                              ),
+                              onPressed: _submit,
+                              child: Text(
+                                _isLogin ? 'Login' : 'Signup',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .surfaceContainer),
+                              ),
                             ),
-                            onPressed: () {},
-                            child: Text(
-                              _isLogin ? 'Login' : 'Signup',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .surfaceContainer),
+                            const SizedBox(height: 8),
+                            Visibility(
+                              visible: _isLogin,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: const Text('Forgot my password?'),
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 8),
-                          Visibility(
-                            visible: _isLogin,
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Forgot my password?'),
+                            Visibility(
+                              visible: _isLogin,
+                              child: Text(
+                                'Or',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onSurface),
+                              ),
                             ),
-                          ),
-                          Visibility(
-                            visible: _isLogin,
-                            child: Text(
-                              'Or',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurface),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(_isLogin
+                                  ? 'Create an account'
+                                  : 'I already have an account'),
                             ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _isLogin = !_isLogin;
-                              });
-                            },
-                            child: Text(_isLogin
-                                ? 'Create an account'
-                                : 'I already have an account'),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
