37f8f997826bf05173d4010981c5a39a391e5c92
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-14 15:29:07 +0300

    firebase secret project details updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:chat_app/screen/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'theme/dark_theme.dart';
 import 'theme/light_theme.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
