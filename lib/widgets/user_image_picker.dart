7ee500cba2957ded431af968f803e5a881881583
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-16 00:26:00 +0300

    image picker widget redesigned

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -35,34 +35,38 @@ class _UserImagePickerState extends State<UserImagePicker> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CircleAvatar(
-          radius: 40,
-          backgroundColor: Colors.grey,
-          backgroundImage:
-              _pickedImage != null ? FileImage(_pickedImage!) : null,
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                width: 2,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.grey,
+            backgroundImage:
+                _pickedImage != null ? FileImage(_pickedImage!) : null,
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 2,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
+          ),
+          TextButton.icon(
+            onPressed: _pickImage,
+            icon: const Icon(Icons.image),
+            label: Text(
+              'Add Image',
+              style: TextStyle(
                 color: Theme.of(context).colorScheme.primary,
               ),
             ),
           ),
-        ),
-        TextButton.icon(
-          onPressed: _pickImage,
-          icon: const Icon(Icons.image),
-          label: Text(
-            'Add Image',
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
