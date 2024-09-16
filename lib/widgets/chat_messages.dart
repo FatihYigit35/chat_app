8da3f6f7e5788967c698fef3910577d6e1faba6d
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-16 23:58:48 +0300

    List of sent messages provided

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/models/chat_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +7,44 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No message found'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: true,
+          )
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No message found.'),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final chatMessages = snapshot.data!.docs;
+        return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 30, right: 10, left: 10),
+          reverse: true,
+          itemCount: chatMessages.length,
+          itemBuilder: (context, index) {
+            final chatModel = ChatModel.fromMap(chatMessages[index].data());
+            return Text(chatModel.text);
+          },
+        );
+      },
     );
   }
 }
