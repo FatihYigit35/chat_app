0f928c4fcfedb427cd998ae20f26aa2f171de1aa
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-16 23:29:05 +0300

    new messages are saved to the database

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,87 @@
+import 'package:chat_app/models/chat_model.dart';
+import 'package:chat_app/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../constant/constants.dart';
+
+final _firestore = FirebaseFirestore.instance;
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() async {
+    final enteredMessage = _messageController.text;
+
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+
+    FocusScope.of(context).unfocus();
+    _messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      return;
+    }
+
+    final userData = await _firestore.collection('users').doc(user.uid).get();
+    if (userData.data() == null) {
+      return;
+    }
+
+    final userModel = UserModel.fromMap(userData.data()!);
+
+    final chatModel = ChatModel(
+      text: enteredMessage,
+      userId: user.uid,
+      userName: userModel.userName,
+      userImage: userModel.imageProfile,
+    );
+
+    _firestore.collection('chat').add(chatModel.toMap());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 1, bottom: 12),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: InputDecoration(
+                labelText: 'Send a message...',
+                border: borderTextFormField,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ],
+      ),
+    );
+  }
+}
